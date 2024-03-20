6a5a6fad6ad157e49609dc481099843651bef5ee
henrie1394 <henry.etuk13@gmail.com>
2024-03-20 19:06:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const hostname = "localhost";
+const port = 8900;
+
+const server = http.createServer((req, res) => {
+  // res.statusCode = 200;
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // using if else
+  if (req.url === "/") {
+    res.write("Home");
+    res.end(); 
+  }
+
+  else if (req.method === "GET" && req.url === "/books") {
+    res.write("hello from GET/books");
+    res.end();
+  } 
+  
+  else if (req.method === "POST" && req.url === "/books") {
+    res.write("hello from POST/books");
+    res.end();
+  } 
+  
+  else if (req.method === "PUT" && req.url === "/books") {
+    res.write("hello from PUT/books");
+    res.end();
+  }
+  
+  else if (req.method === "DELETE" && req.url === "/books") {
+    res.write("Hello from DELETE/books");
+    res.end();
+  } 
+  
+  else if (req.method === "GET" && req.url === "/books/author/") {
+    res.write("Hellow from GET/books/author");
+    res.end();
+  }
+  
+  else if (req.method === "POST" && req.url === "/books/author/") {
+    res.write("Hellow from POST/books/author/");
+    res.end();
+  }
+  
+  else if (req.method === "PUT" && req.url === "/books/author/") {
+    res.write("Hello from PUT/books/author");
+    res.end();
+  } 
+  
+  else {
+    // res.writeHead(404, {'Content-Type':'text/plain'});
+    res.write("404 page not found");
+    res.end();
+  }
+
+});
+
+server.listen(port, function () {
+  console.log(`Server is running at http://${hostname}:${port}/`);
+});
